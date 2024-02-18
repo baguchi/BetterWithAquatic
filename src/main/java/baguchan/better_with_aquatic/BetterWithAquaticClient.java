2cf7f46305d012d12662f6e7093935963fd1a2c9
baguchan <shutux500@gmail.com>
2024-02-18 21:38:09 +0900

    2.2.1

--- src/main/java/baguchan/better_with_aquatic/BetterWithAquaticClient.java
+++ src/main/java/baguchan/better_with_aquatic/BetterWithAquaticClient.java
@@ -11,16 +11,22 @@ import net.minecraft.client.render.block.model.BlockModelRenderBlocks;
 import turniplabs.halplibe.helper.EntityHelper;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
 import useless.dragonfly.helper.ModelHelper;
+import useless.dragonfly.model.entity.BenchEntityModel;
 
 
 public class BetterWithAquaticClient implements ClientStartEntrypoint {
+
+	public static final BenchEntityModel modelDrowned = ModelHelper.getOrCreateEntityModel(BetterWithAquatic.MOD_ID, "entity/drowned.geo.json", DrownedModel.class);
+
+	public static final BenchEntityModel modelFrog = ModelHelper.getOrCreateEntityModel(BetterWithAquatic.MOD_ID, "entity/frog.geo.json", FrogModel.class);
+
 
 	@Override
 	public void beforeClientStart() {
 		EntityHelper.Client.assignEntityRenderer(EntityFish.class, new RenderFish(new FishModel(), 0.3F));
 		EntityHelper.Client.assignEntityRenderer(EntityAnglerFish.class, new RenderAnglerFish(new AnglerFishModel(), 0.4F));
-		EntityHelper.Client.assignEntityRenderer(EntityDrowned.class, new DrownedRenderer(ModelHelper.getOrCreateEntityModel(BetterWithAquatic.MOD_ID, "entity/drowned.geo.json", DrownedModel.class), 0.5f));
-		EntityHelper.Client.assignEntityRenderer(EntityFrog.class, new FrogRenderer(ModelHelper.getOrCreateEntityModel(BetterWithAquatic.MOD_ID, "entity/frog.geo.json", FrogModel.class), 0.3f));
+		EntityHelper.Client.assignEntityRenderer(EntityDrowned.class, new DrownedRenderer(modelDrowned, 0.5f));
+		EntityHelper.Client.assignEntityRenderer(EntityFrog.class, new FrogRenderer(modelFrog, 0.3f));
 
 	}
 
